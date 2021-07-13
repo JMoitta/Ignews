c48cc686f44a5a1a22e2474a949cbd3b1f63c9bd
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2021-07-13 10:16:59 -0300

    Páginas estáticas

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from '@prismicio/client'
+import Link from "next/link";
 import { getPrismicClient } from "../../services/prismic";
 import styles from './styles.module.scss'
 import { RichText } from "prismic-dom";
+import { useSession } from "next-auth/client";
+import { useCallback } from "react";
 
 type Post = {
   slug: string;
@@ -17,6 +20,13 @@ interface PostsProps {
 }
 
 export default function Posts({ posts }: PostsProps) {
+  const [session] = useSession()
+
+  const urlPath = useCallback(
+    (slug: string) => {
+      return session ? `/posts/${slug}` : `/posts/preview/${slug}`
+    }, [session]);
+
   return (
     <>
       <Head>
@@ -26,11 +36,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={urlPath(post.slug)} >
+              <a >
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from "stripe";
-import { version } from '../../package.json'
+import Package from '../../package.json'
+
+const { version } = Package
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
